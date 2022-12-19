c9b8b12029074584e4d04349c5726f8b25eddcae
Abdulkadir Adow <abdul.adow1@gmail.com>
2022-12-19 23:27:57 +0000

    "simple"

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ let todo1 = "Groceries";
 let todo2 = "Wash the car";
 let todo3 = "Make dinner";
 
-console.log (todo1);
-typeof todo1
+
+
+function addTodo(todoTitle) {
+  let element = document.createElement ("div");
+  element.innerText = todoTitle;
+  document.body.appendChild(element);
+}
+addTodo(todo1);
+addTodo(todo2);
+addTodo(todo3);
+
+
+
+ 
